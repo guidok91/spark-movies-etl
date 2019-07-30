c8a639913055ed196987a97525894f27e125947b
Guido Kosloff Gancedo <guidok_91@hotmail.com>
2019-07-30 19:49:57 +0200

    Add transform tests

---
+++ tests/tasks/transform_data_task_test.py
@@ -0,0 +1,24 @@
+from sparktestingbase.sqltestcase import SQLTestCase
+from programs.tasks.transform_data_task import TransformDataTask
+from tests.tasks.fixtures.expected_output import TRANSFORMED_EXPECTED_OUTPUT, TRANSFORMED_EXPECTED_SCHEMA
+from os import path
+
+
+class TransformDataTaskTest(SQLTestCase):
+    def setUp(self):
+        SQLTestCase.setUp(self)
+        self.transformation = TransformDataTask._transform
+
+    def test_transform(self):
+        df_input = self.sqlCtx.read.json(
+            "file:///" + path.dirname(path.abspath(__file__)) + "/fixtures/sample_movies.json"
+        )
+        df_expected_output = self.sqlCtx.createDataFrame(
+            TRANSFORMED_EXPECTED_OUTPUT,
+            schema=TRANSFORMED_EXPECTED_SCHEMA
+        )
+        df_output = self.transformation(df_input)
+        self.assertDataFrameEqual(
+            df_output.orderBy(df_output.columns),
+            df_expected_output.orderBy(df_expected_output.columns)
+        )
