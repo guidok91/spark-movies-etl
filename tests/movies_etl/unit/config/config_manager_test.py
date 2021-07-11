d8d322d2665ff9137c7d9a3c4581f6cd5b8e445b
GK <guidokosloff@gmail.com>
2021-07-11 14:19:27 +0200

    Add error handling for inexistent config file + tests

---
+++ tests/movies_etl/unit/config/config_manager_test.py
@@ -0,0 +1,39 @@
+import os
+from unittest import TestCase
+from movies_etl.config.config_manager import ConfigManager, ConfigException
+
+
+class TestConfigManager(TestCase):
+
+    def test_read_existent_value(self):
+        # GIVEN
+        config_manager = ConfigManager(
+            config_file=f"{os.path.dirname(os.path.realpath(__file__))}/../fixtures/config_test.yaml"
+        )
+        value_expected = "v1"
+
+        # WHEN
+        value_output = config_manager.get("k1")
+
+        # THEN
+        self.assertEqual(value_output, value_expected)
+
+    def test_read_inexistent_value(self):
+        # GIVEN
+        config_manager = ConfigManager(
+            config_file=f"{os.path.dirname(os.path.realpath(__file__))}/../fixtures/config_test.yaml"
+        )
+
+        # THEN
+        with self.assertRaises(ConfigException):
+            config_manager.get("k2")
+
+    def test_inexistent_config_file(self):
+        # GIVEN
+        inexistent_config_file = "inexistent_config_file.yaml"
+
+        # THEN
+        with self.assertRaises(FileNotFoundError):
+            ConfigManager(
+                config_file=inexistent_config_file
+            )
