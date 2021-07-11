4b0d648b5d30562d5311f6879153e0b39c2a8657
GK <guidokosloff@gmail.com>
2021-07-11 14:28:33 +0200

    Refactor TestExecutor

--- tests/movies_etl/integration/executor_test.py
+++ tests/movies_etl/integration/executor_test.py
@@ -18,7 +18,24 @@ class TestExecutor(TestCase):
     def tearDown(self) -> None:
         self.spark.stop()
 
-    def test_executor_run_ingest(self) -> None:
+    def test_run_end_to_end(self):
+        self._test_run_ingest()
+        self._test_run_transform()
+
+    def test_executor_run_inexistent_task(self) -> None:
+        # GIVEN
+        executor = Executor(
+            spark=self.spark,
+            config_manager=self.config_manager,
+            task="inexistent_task",
+            execution_date=self.execution_date,
+        )
+
+        # THEN
+        with self.assertRaises(ConfigException):
+            executor.run()
+
+    def _test_run_ingest(self) -> None:
         # GIVEN
         executor = Executor(
             spark=self.spark, config_manager=self.config_manager, task="ingest", execution_date=self.execution_date
@@ -39,7 +56,7 @@ class TestExecutor(TestCase):
         df_output = self.spark.read.parquet(self.config_manager.get("data_lake.standardised"))
         assert_data_frames_equal(df_output, df_expected)
 
-    def test_executor_run_transform(self) -> None:
+    def _test_run_transform(self) -> None:
         # GIVEN
         executor = Executor(
             spark=self.spark, config_manager=self.config_manager, task="transform", execution_date=self.execution_date
@@ -55,16 +72,3 @@ class TestExecutor(TestCase):
         # THEN
         df_output = self.spark.read.parquet(self.config_manager.get("data_lake.curated"))
         assert_data_frames_equal(df_output, df_expected)
-
-    def test_executor_run_inexistent_task(self) -> None:
-        # GIVEN
-        executor = Executor(
-            spark=self.spark,
-            config_manager=self.config_manager,
-            task="inexistent_task",
-            execution_date=self.execution_date,
-        )
-
-        # THEN
-        with self.assertRaises(ConfigException):
-            executor.run()
