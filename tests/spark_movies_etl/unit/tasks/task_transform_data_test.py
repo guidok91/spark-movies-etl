9c31d0996bd0b2df77b758b8f5372609eba34308
Guido Kosloff Gancedo <gkg@hellofresh.com>
2021-12-03 13:36:23 +0100

    Fix transformation unit test

--- tests/spark_movies_etl/unit/tasks/task_transform_data_test.py
+++ tests/spark_movies_etl/unit/tasks/task_transform_data_test.py
@@ -1,37 +1,28 @@
-from unittest import TestCase
+from pyspark.sql import SparkSession
 
 from spark_movies_etl.schema import Schema
 from spark_movies_etl.tasks.task_transform_data import Transformation
-from tests.spark_movies_etl.unit.fixtures.data import (
+from tests.conftest import assert_data_frames_equal
+from tests.spark_movies_etl.unit.tasks.fixtures.data import (
     TEST_TRANSFORMATION_INPUT,
     TEST_TRANSFORMATION_OUTPUT_EXPECTED,
 )
-from tests.utils import assert_data_frames_equal, get_local_spark
 
 
-class TestTransformation(TestCase):
-    def setUp(self) -> None:
-        self.spark = get_local_spark()
+def test_transform(spark: SparkSession) -> None:
+    # GIVEN
+    transformation = Transformation(movies_regions=["FR", "US", "GB", "RU", "HU", "DK", "ES"], movies_max_reissues=5)
+    df_input = spark.createDataFrame(
+        TEST_TRANSFORMATION_INPUT,  # type: ignore
+        schema=Schema.SILVER,
+    )
+    df_expected = spark.createDataFrame(
+        TEST_TRANSFORMATION_OUTPUT_EXPECTED,  # type: ignore
+        schema=Schema.GOLD,
+    )
 
-    def tearDown(self) -> None:
-        self.spark.stop()
+    # WHEN
+    df_transformed = transformation.transform(df_input)
 
-    def test_transform(self) -> None:
-        # GIVEN
-        transformation = Transformation(
-            movies_regions=["FR", "US", "GB", "RU", "HU", "DK", "ES"], movies_max_reissues=5
-        )
-        df_input = self.spark.createDataFrame(
-            TEST_TRANSFORMATION_INPUT,  # type: ignore
-            schema=Schema.SILVER,
-        )
-        df_expected = self.spark.createDataFrame(
-            TEST_TRANSFORMATION_OUTPUT_EXPECTED,  # type: ignore
-            schema=Schema.GOLD,
-        )
-
-        # WHEN
-        df_transformed = transformation.transform(df_input)
-
-        # THEN
-        assert_data_frames_equal(df_transformed, df_expected)
+    # THEN
+    assert_data_frames_equal(df_transformed, df_expected)
