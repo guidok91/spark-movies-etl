3bff017ef9bcd1afbaed6d22ba9b1d8453a91ca8
Guido Kosloff Gancedo <gkg@hellofresh.com>
2021-12-03 13:29:50 +0100

    Fix config unit test

--- tests/spark_movies_etl/unit/config/config_manager_test.py
+++ tests/spark_movies_etl/unit/config/config_manager_test.py
@@ -1,37 +1,38 @@
 import os
-from unittest import TestCase
+import pytest
 
 from spark_movies_etl.config.config_manager import ConfigException, ConfigManager
 
 
-class TestConfigManager(TestCase):
-    def test_read_existent_key(self) -> None:
-        # GIVEN
-        config_manager = ConfigManager(
-            config_file=f"{os.path.dirname(os.path.realpath(__file__))}/../fixtures/config_test.yaml"
-        )
-        value_expected = "v1"
+def test_read_existent_key() -> None:
+    # GIVEN
+    config_manager = ConfigManager(
+        config_file=f"{os.path.dirname(os.path.realpath(__file__))}/fixtures/config_test.yaml"
+    )
+    value_expected = "v1"
 
-        # WHEN
-        value_output = config_manager.get("k1")
+    # WHEN
+    value_output = config_manager.get("k1")
 
-        # THEN
-        self.assertEqual(value_output, value_expected)
+    # THEN
+    assert value_output == value_expected
 
-    def test_read_inexistent_key(self) -> None:
-        # GIVEN
-        config_manager = ConfigManager(
-            config_file=f"{os.path.dirname(os.path.realpath(__file__))}/../fixtures/config_test.yaml"
-        )
 
-        # THEN
-        with self.assertRaises(ConfigException):
-            config_manager.get("inexistent_key")
+def test_read_inexistent_key() -> None:
+    # GIVEN
+    config_manager = ConfigManager(
+        config_file=f"{os.path.dirname(os.path.realpath(__file__))}/fixtures/config_test.yaml"
+    )
 
-    def test_inexistent_config_file(self) -> None:
-        # GIVEN
-        inexistent_config_file = "inexistent_config_file.yaml"
+    # THEN
+    with pytest.raises(ConfigException):
+        config_manager.get("inexistent_key")
 
-        # THEN
-        with self.assertRaises(FileNotFoundError):
-            ConfigManager(config_file=inexistent_config_file)
+
+def test_inexistent_config_file() -> None:
+    # GIVEN
+    inexistent_config_file = "inexistent_config_file.yaml"
+
+    # THEN
+    with pytest.raises(FileNotFoundError):
+        ConfigManager(config_file=inexistent_config_file)
