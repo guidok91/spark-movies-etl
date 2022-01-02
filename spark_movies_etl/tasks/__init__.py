2a4fa1e1518261af14e94c3c0cd5f256bddd3f48
GK <guidokosloff@gmail.com>
2022-01-02 16:41:12 -0300

    Add tasks to __init__

---
+++ spark_movies_etl/tasks/__init__.py
@@ -0,0 +1,4 @@
+from spark_movies_etl.tasks.curate_data.task import CurateDataTask
+from spark_movies_etl.tasks.standardize_data.task import StandardizeDataTask
+
+__all__ = ["CurateDataTask", "StandardizeDataTask"]
