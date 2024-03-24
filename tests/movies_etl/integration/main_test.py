0a5b229592cae32d79decaa20a0e7771b6233eff
guidok91 <guidokosloff@gmail.com>
2024-03-24 11:40:18 +0100

    Add more error test cases

--- tests/movies_etl/integration/main_test.py
+++ tests/movies_etl/integration/main_test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import pytest
+from pyspark.errors.exceptions.captured import AnalysisException
 from pyspark.sql import SparkSession
 
 from movies_etl.main import main
@@ -9,6 +11,21 @@ from movies_etl.main import main
 def test_run_end_to_end_idempotent(spark: SparkSession) -> None:
     _test_run(spark)
     _test_run(spark)
+
+
+def test_run_error_inexistent_source_path(spark: SparkSession) -> None:
+    # GIVEN
+    sys.argv = [
+        "main.py",
+        "--execution-date",
+        "2020-01-01",
+        "--config-file-path",
+        f"{os.path.dirname(os.path.abspath(__file__))}/fixtures/test_app_config.yaml",
+    ]
+
+    # THEN
+    with pytest.raises(AnalysisException, match=r".*Path does not exist.*"):
+        main()
 
 
 def _test_run(spark: SparkSession) -> None:
