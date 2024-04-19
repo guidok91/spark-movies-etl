f52094003144e297f5aeb1f12eb82376ea5c8135
guidok91 <guidokosloff@gmail.com>
2024-04-19 09:15:12 -0300

    Refactor path to fixtures

--- tests/movies_etl/unit/tasks/curate_data_transformation_test.py
+++ tests/movies_etl/unit/tasks/curate_data_transformation_test.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 
 import pytest
@@ -7,12 +8,14 @@ from pyspark.sql import SparkSession
 from movies_etl.tasks.curate_data_transformation import CurateDataTransformation
 from tests.movies_etl.conftest import assert_data_frames_equal
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
 
 def test_transform(spark: SparkSession) -> None:
     # GIVEN
     transformation = CurateDataTransformation(execution_date=date(2021, 1, 1))
-    df_input = spark.read.json("tests/movies_etl/unit/tasks/fixtures/test_transform_input.ndjson")
-    df_expected = spark.read.json("tests/movies_etl/unit/tasks/fixtures/test_transform_output_expected.ndjson")
+    df_input = spark.read.json(f"{CURRENT_DIR}/fixtures/test_transform_input.ndjson")
+    df_expected = spark.read.json(f"{CURRENT_DIR}/fixtures/test_transform_output_expected.ndjson")
 
     # WHEN
     df_transformed = transformation.transform(df_input)
@@ -24,7 +27,7 @@ def test_transform(spark: SparkSession) -> None:
 def test_transform_missing_column(spark: SparkSession) -> None:
     # GIVEN
     transformation = CurateDataTransformation(execution_date=date(2021, 1, 1))
-    df_input = spark.read.json("tests/movies_etl/unit/tasks/fixtures/test_transform_missing_column_input.ndjson")
+    df_input = spark.read.json(f"{CURRENT_DIR}/fixtures/test_transform_missing_column_input.ndjson")
 
     # THEN
     with pytest.raises(
