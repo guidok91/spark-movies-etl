c8c1de9ca7ad9f8351a8fc1594da5ce1d7602666
Guido Kosloff Gancedo <38698125+guidok91@users.noreply.github.com>
2024-08-09 11:23:34 +0200

    Upgrade deps (#91)

--- tests/movies_etl/conftest.py
+++ tests/movies_etl/conftest.py
@@ -9,7 +9,7 @@ from pyspark.sql import DataFrame, SparkSession
 def spark() -> Generator:
     spark = (
         SparkSession.builder.master("local[*]")
-        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.1.0")
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
         .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
