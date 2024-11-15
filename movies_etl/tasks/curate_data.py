e8de77db9cf718190a08ca8d8253a4345cdb6c20
guidok91 <guidokosloff@gmail.com>
2024-11-15 11:54:14 +0100

    Reintroduce dq checks

--- movies_etl/tasks/curate_data.py
+++ movies_etl/tasks/curate_data.py
@@ -2,10 +2,9 @@ import datetime
 import os
 
 from pyspark.sql import DataFrame, SparkSession
+from soda.scan import Scan
 
 from movies_etl.tasks.curate_data_transformation import CurateDataTransformation
-
-# from soda.scan import Scan
 
 
 class CurateDataTask:
@@ -21,8 +20,7 @@ class CurateDataTask:
         df = self._read_input()
         df_transformed = self._transform(df)
         self._write_output(df_transformed)
-        # TO DO
-        # self._run_data_quality_checks()
+        self._run_data_quality_checks()
 
     def _read_input(self) -> DataFrame:
         input_path = os.path.join(self.path_input, self.execution_date.strftime("%Y/%m/%d"))
@@ -37,23 +35,20 @@ class CurateDataTask:
         self.logger.info(f"Writing output data to {self.path_output}.")
         df.write.format("delta").partitionBy(["run_date"]).mode("overwrite").save(self.path_output)
 
-    # def _run_data_quality_checks(self) -> None:
-    #     self.logger.info(f"Running Data Quality checks for table {self.path_output}.")
-    #     dq_checks_config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "curate_data_checks.yaml")
-    #     scan = Scan()
+    def _run_data_quality_checks(self) -> None:
+        self.logger.info(f"Running Data Quality checks for output ({self.path_output}).")
+        self.spark.read.format("delta").load(self.path_output).createOrReplaceTempView("movie_ratings_curated")
 
-    #     scan.set_data_source_name("spark_df")
-    #     scan.add_spark_session(self.spark)
-    #     scan.add_variables(
-    #         {
-    #             "table": self.path_output,
-    #             "run_date": self.execution_date.strftime("%Y-%m-%d"),
-    #         }
-    #     )
-    #     scan.add_sodacl_yaml_file(dq_checks_config_file)
+        dq_checks_config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "curate_data_checks.yaml")
+        scan = Scan()
 
-    #     scan.execute()
+        scan.set_data_source_name("spark_df")
+        scan.add_spark_session(self.spark)
+        scan.add_variables({"run_date": self.execution_date.strftime("%Y-%m-%d")})
+        scan.add_sodacl_yaml_file(dq_checks_config_file)
 
-    #     self.logger.info(scan.get_scan_results())
-    #     scan.assert_no_error_logs()
-    #     scan.assert_no_checks_fail()
+        scan.execute()
+
+        self.logger.info(scan.get_scan_results())
+        scan.assert_no_error_logs()
+        scan.assert_no_checks_fail()
