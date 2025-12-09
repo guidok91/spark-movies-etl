25a5c8630d65a42dcf5a81fbda9e0d2c528bb262
Guido Kosloff Gancedo <38698125+guidok91@users.noreply.github.com>
2025-12-09 16:31:18 +0100

    Simplify transformation methods (#139)

--- movies_etl/tasks/curate_data/transformation.py
+++ movies_etl/tasks/curate_data/transformation.py
@@ -8,17 +8,20 @@ from pyspark.sql.window import Window
 class CurateDataTransformation:
     def transform(self, df: DataFrame) -> DataFrame:
         transformations = (
-            self._normalize_columns,
+            self._transform_columns,
             self._remove_duplicates,
-            self._calculate_multigenre,
             self._select_final_columns,
         )
 
         return reduce(DataFrame.transform, transformations, df)  # type: ignore
 
     @staticmethod
-    def _normalize_columns(df: DataFrame) -> DataFrame:
-        return df.withColumn("is_adult", col("adult")).withColumn("original_language", upper("original_language"))
+    def _transform_columns(df: DataFrame) -> DataFrame:
+        return (
+            df.withColumn("is_adult", col("adult"))
+            .withColumn("original_language", upper("original_language"))
+            .withColumn("is_multigenre", size("genres") > 1)
+        )
 
     @staticmethod
     def _remove_duplicates(df: DataFrame) -> DataFrame:
@@ -26,10 +29,6 @@ class CurateDataTransformation:
         window_spec = Window.partitionBy(["rating_id"]).orderBy("timestamp")
         df = df.withColumn("rnum", row_number().over(window_spec))
         return df.where(col("rnum") == 1).drop("rnum")
-
-    @staticmethod
-    def _calculate_multigenre(df: DataFrame) -> DataFrame:
-        return df.withColumn("is_multigenre", size("genres") > 1)
 
     @staticmethod
     def _select_final_columns(df: DataFrame) -> DataFrame:
